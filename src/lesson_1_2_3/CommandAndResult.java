e26c41de9cc87a7045771df41cf69a7433b204e7
Полина <ankudinovapol@gmail.com>
2022-07-16 03:54:25 +0500

    basik

---
+++ src/lesson_1_2_3/CommandAndResult.java
@@ -0,0 +1,5 @@
+package lesson_1_2_3;
+
+public class CommandAndResult {
+
+}
---
+++ src/lesson_1_2_3/Main.java
@@ -0,0 +1,9 @@
+package lesson_1_2_3;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
