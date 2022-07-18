735ee350adc813c8494672538595212e0936d596
Полина <ankudinovapol@gmail.com>
2022-07-18 21:56:55 +0500

    lessons_7_8_9

--- src/lesson_1_2_3/HR.java
+++ src/lesson_1_2_3/HR.java
@@ -1,12 +1,16 @@
 package lesson_1_2_3;
 
 public class HR {
-    public static void acceptCandidate(String name, int salary){
+    public static void acceptSalary(String name, long salary){
         Main.print(name + " want to receive " + salary  + " dollars");
     }
 
-    public static void acceptCandidate(String name){
-        Main.print(name + " is accepted\n");
+    public static void acceptCandidate(String name, long salary){
+        if (salary <= Integer.MAX_VALUE){
+            Main.print(name + " is accepted\n");
+        } else{
+            Main.print(name + " isn't accepted\n");
+        }
     }
 
 }
--- src/lesson_1_2_3/Main.java
+++ src/lesson_1_2_3/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         infoForCandidate("Polina", 10000, 15);
-        infoForCandidate("John", 200000, 10);
+        infoForCandidate("John", 20000000000000L, 10);
     }
 
-    public static void infoForCandidate(String name,int salary,  int time){
+    public static void infoForCandidate(String name,long salary,  int time){
         Manager.interviewTime(name, time);
+        HR.acceptSalary(name, salary);
         HR.acceptCandidate(name, salary);
-        HR.acceptCandidate(name);
     }
     public static void print(String text){
         System.out.println(text);
