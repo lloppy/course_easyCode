800d80879e3b3fce68b093908faf2e0dbc2ba746
Полина <ankudinovapol@gmail.com>
2022-07-19 11:10:59 +0500

    Lets go drink beer!

---
+++ src/lesson_10_11_12/Main.java
@@ -0,0 +1,28 @@
+package lesson_10_11_12;
+
+public class Main {
+
+    public static void main(String[] args) {
+        print(drink(true, true));
+        print(drink(true, false));
+        print(drink(false, true));
+        print(drink(false, false));
+
+    }
+
+    public static String drink(boolean isTodayFriday, boolean aLotOfMoney){
+        if(isTodayFriday && aLotOfMoney){
+            print("Lets go drink beer!");
+        } else {
+            if (isTodayFriday){
+                print("Need to ask for a loan");
+            } else {
+                print("Buckwheat is our everything(");
+            }
+        }
+        return "";
+    }
+    public static void print(String text) {
+        System.out.println(text);
+    }
+}
