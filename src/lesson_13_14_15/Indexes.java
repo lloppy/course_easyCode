dc010dec2902ac6c9516b36061da51c2dcfbaee6
Полина <ankudinovapol@gmail.com>
2022-07-21 23:32:26 +0500

    rewrite find stroke

--- src/lesson_13_14_15/Indexes.java
+++ src/lesson_13_14_15/Indexes.java
@@ -4,16 +4,23 @@ public class Indexes {
     public static void main(String[] args) {
         findStroke("this", new String[]{"this", "that", "those"});
         findStroke("those", new String[]{"this", "that", "those"});
+        findStroke("those", new String[]{"this", "that", "those", "those", "those", "those"});
         findStroke("those", new String[]{});
     }
 
     private static void findStroke(String stoke, String[] array) {
         if (array.length > 0) {
+            StringBuilder indexes  = new StringBuilder();
             for (int i = 0; i < array.length; i++) {
                 if (array[i].equals(stoke)) {
-                    System.out.println("We've find " + stoke + "! It's got " + i + " index!");
+                    if(indexes.length() > 0){
+                        indexes.append(", ");
+                    }
+                    indexes.append(i);
                 }
             }
+            System.out.println("We've find " + stoke + "! It's got " + indexes + " index!");
+
         } else {
             System.out.println("Your array is empty(");
 
