5f29df8ebcc1f5247758668645f6edf57fd4c88d
Полина <ankudinovapol@gmail.com>
2022-07-21 23:04:20 +0500

    refactor

--- src/lesson_13_14_15/Reverse.java
+++ src/lesson_13_14_15/Reverse.java
@@ -1,19 +1,16 @@
 package lesson_13_14_15;
 
-    public class Reverse {
+public class Reverse {
     public static void main(String[] args) {
-       reverseNum(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
-
-
+        reverseNum(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
     }
 
-        public static void reverseNum(int[] array) {
-            for(int i = array.length -1 ; i >=0 ; i--){
-                System.out.println(array[i]);
-                if(i==0){
-                    System.out.println("End of array!");
-                }
+    public static void reverseNum(int[] array) {
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.println(array[i]);
+            if (i == 0) {
+                System.out.println("End of array!");
             }
         }
-
+    }
 }
