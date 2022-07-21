c8c8df193c128a105554f5cf7b36fc62943d22be
Полина <ankudinovapol@gmail.com>
2022-07-21 23:03:50 +0500

    Reverse count method

--- src/lesson_13_14_15/Reverse.java
+++ src/lesson_13_14_15/Reverse.java
@@ -1,15 +1,19 @@
 package lesson_13_14_15;
 
-public class Reverse {
+    public class Reverse {
     public static void main(String[] args) {
-        int[] arrayReverse = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+       reverseNum(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
 
-        for(int i = arrayReverse.length -1 ; i >=0 ; i--){
-            System.out.println(arrayReverse[i]);
-            if(i==0){
-                System.out.println("End of array!");
+
+    }
+
+        public static void reverseNum(int[] array) {
+            for(int i = array.length -1 ; i >=0 ; i--){
+                System.out.println(array[i]);
+                if(i==0){
+                    System.out.println("End of array!");
+                }
             }
         }
-    }
 
 }
