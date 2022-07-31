545965d62bbd362ffe2f04910ff059fefdfe5016
Полина <ankudinovapol@gmail.com>
2022-07-31 21:00:38 +0500

    Rectangle and square

--- src/lesson_18_19_20/Main.java
+++ src/lesson_18_19_20/Main.java
@@ -1,4 +1,28 @@
 package lesson_18_19_20;
 
 public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Polina", "Ankudinova");
+        Person person2 = new Person("Porina", "Shramko");
+
+        MemoryId memoryId = new MemoryId(777, "My name is Bond, James Bond");
+
+        Lesson lesson = new Lesson(5, true);
+        Lesson lesson2 = new Lesson(1, false);
+
+        Rectangle rectangle = new Rectangle(3, 3, 1, 3);
+        Rectangle rectangle2 = new Rectangle(13, 1, 13, 2);
+        System.out.println(rectangle.getDecscription());
+        System.out.println(rectangle2.getDecscription());
+
+        Rectangle square = new Rectangle(
+                new Point(0, 2),
+                new Point(0, 4),
+                new Point(2, 4),
+                new Point(2, 2)
+        );
+        System.out.println(square.getDecscription());
+
+
+    }
 }
--- src/lesson_18_19_20/Rectangle.java
+++ src/lesson_18_19_20/Rectangle.java
@@ -6,6 +6,9 @@ public class Rectangle {
     private final int sideB;
     private final int sideC;
     private final int sideD;
+    public Rectangle(Point a, Point b, Point c, Point d){
+        this(a.getDistance(b), b.getDistance(c), c.getDistance(d), d.getDistance(a) );
+    }
 
     public Rectangle(int sideA, int sideB, int sideC, int sideD) {
         this.sideA = sideA;
@@ -13,9 +16,22 @@ public class Rectangle {
         this.sideC = sideC;
         this.sideD = sideD;
     }
-
-    public boolean isSquare() {
+    private boolean isSquare() {
         return this.sideA == this.sideB && this.sideC == this.sideD &&
                 this.sideB == this.sideC;
+    }
+
+
+    public static void createRec(int pointA, int pointB, int pointC, int pointD){
+        
+
+    }
+
+    public String getDecscription() {
+        return isSquare()?
+                "Square":
+                "Rectangle";
+
+
     }
 }
